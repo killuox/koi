e71836d6c5d617c1db53ef08dcc5601ddcb512cd
Antonin Arsenault <anto94@live.ca>
2025-10-10 23:14:05 -0400

    update bin

--- bin/run.js
+++ bin/run.js
@@ -6,7 +6,7 @@ const platform = process.platform;
 let binary;
 
 if (platform === "win32") binary = "koi_windows_amd64_v1";
-else if (platform === "darwin") binary = "koi_darwin_am ";
+else if (platform === "darwin") binary = "koi_darwin_amd64_v1";
 else binary = "koi_linux_amd64_v1";
 
 const binaryPath = path.join(__dirname, binary);
