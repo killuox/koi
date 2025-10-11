3f62082984471977d095f5d250672012dfa22c5f
Antonin Arsenault <anto94@live.ca>
2025-10-10 23:24:54 -0400

    test linux bin

--- bin/run.js
+++ bin/run.js
@@ -5,9 +5,9 @@ const path = require("path");
 const platform = process.platform;
 let binary;
 
-if (platform === "win32") binary = "koi_windows_amd64_v1";
-else if (platform === "darwin") binary = "koi_darwin_amd64_v1";
-else binary = "koi_linux_amd64_v1";
+if (platform === "win32") binary = "koi.exe";
+else if (platform === "darwin") binary = "koi-macos";
+else binary = "koi-linux";
 
 const binaryPath = path.join(__dirname, binary);
 
