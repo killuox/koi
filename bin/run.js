e0cd961ccff3caaa52247a8bfa9c4b0db5fd12f1
Antonin Arsenault <anto94@live.ca>
2025-10-10 23:32:37 -0400

    Update run.js

--- bin/run.js
+++ bin/run.js
@@ -1,15 +1,29 @@
 #!/usr/bin/env node
 const { spawn } = require("child_process");
 const path = require("path");
+const fs = require("fs");
 
 const platform = process.platform;
-let binary;
+let binaryFolder, binaryName;
 
-if (platform === "win32") binary = "koi.exe";
-else if (platform === "darwin") binary = "koi-macos";
-else binary = "koi-linux";
+if (platform === "win32") {
+  binaryFolder = "koi_windows_amd64_v1";
+  binaryName = "koi.exe";
+} else if (platform === "darwin") {
+  binaryFolder = "koi_darwin_amd64_v1";
+  binaryName = "koi";
+} else {
+  binaryFolder = "koi_linux_amd64_v1";
+  binaryName = "koi";
+}
 
-const binaryPath = path.join(__dirname, binary);
+const binaryPath = path.join(__dirname, binaryFolder, binaryName);
+
+// Make sure binary exists
+if (!fs.existsSync(binaryPath)) {
+  console.error(`Binary not found at ${binaryPath}`);
+  process.exit(1);
+}
 
 const child = spawn(binaryPath, process.argv.slice(2), { stdio: "inherit" });
 child.on("exit", (code) => process.exit(code));
